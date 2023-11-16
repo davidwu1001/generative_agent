fd48b1a1c572b16bd0b72a3a7e5de60250951727
David <131512698+davidwu1001@users.noreply.github.com>
2023-11-16 14:57:28 +0800

    改善task_decomp 解决Qwen不置零

--- reverie/backend_server/persona/prompt_template/run_gpt_prompt.py
+++ reverie/backend_server/persona/prompt_template/run_gpt_prompt.py
@@ -469,7 +469,13 @@ def run_gpt_prompt_task_decomp(persona,
     output = safe_generate_response(prompt, gpt_param, 5, get_fail_safe(), validate, clean_up)
     # if int(output[output.index("minutes left:") + 13:output.index(')')]) < minutes_left:
     previous_items.append(f"{len(previous_items) + 1}) {output}")
+    duration_in_minutes = int(output[output.index("duration in minutes:") + 20:output.index(',')])
     minutes_left = int(output[output.index("minutes left:") + 13:output.index(')')])
+    if minutes_left <= 10:  # 还剩 10min的时候直接把这个子任务置零 解决Qwen不置零问题
+      duration_in_minutes += minutes_left
+      output[output[output.index("duration in minutes:") + 20:output.index(',')]] = duration_in_minutes
+      output[output[output.index("minutes left:") + 13:output.index(')')]] = 0
+
     # else:
     #   pass
 
@@ -2905,7 +2911,7 @@ def run_gpt_generate_iterative_chat_utt(maze, init_persona, target_persona, retr
     return cleaned_dict
 
   def __chat_func_validate(gpt_response, prompt=""): 
-    print ("ugh...")
+    print ("验证对话中")
     try: 
       # print ("debug 1")
       # print (gpt_response)
